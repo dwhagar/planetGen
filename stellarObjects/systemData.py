6c001e767988d764fb0cd7cc886e11639c79da09
David Hagar <david.hagar@gmail.com>
2024-06-21 19:01:18 -0400

    I think I've got the object mostly ready to test.

---
+++ stellarObjects/systemData.py
@@ -0,0 +1,81 @@
+import random
+from starData import Star  # Import your Star class from starData.py
+from planetData import Planet  # Import your Planet class from planetData.py
+
+class StarSystem:
+    """
+    A class representing a star system, containing a central star and a list of planets.
+    """
+
+    def __init__(self):
+        """
+        Initializes a StarSystem object.
+        """
+
+        self.star = Star()
+        self.star.generate_random_star()
+        self.planets = []
+
+        self.system_bounds()
+        system_objects = self.define_system_size()
+
+        for i in range(system_objects):
+            normalized_distance = i / (system_objects - 1)  # Calculate normalized distance
+            estimated_distance = self.estimate_distance(normalized_distance)
+            estimated_distance *= random.uniform(0.5, 2.0)  # Random multiplier for variation
+
+            planet = Planet(self.star.habitable_zone, estimated_distance)
+            self.planets.append(planet)
+
+    def system_bounds(self):
+        """
+        Estimates the inner and outer bounds of a planetary system based on the star's mass.
+        """
+
+        # Constants based on our solar system
+        INNER_LIMIT_FACTOR = 0.05  # Mercury's distance in AU relative to the Sun's mass
+        OUTER_LIMIT_FACTOR = 30.0  # Neptune's distance in AU relative to the Sun's mass
+
+        # Calculate inner and outer limits
+        inner_limit = INNER_LIMIT_FACTOR * self.stellar_mass
+        outer_limit = OUTER_LIMIT_FACTOR * self.stellar_mass
+
+        self.inner_limit = inner_limit
+        self.outer_limit = outer_limit
+
+    def define_system_size(self):
+        """
+        Based on size of the system in AU's, it generates the size of the system by number of
+        stellar objects.
+        """
+        # Density of stellar objects within our solar system as a function of AU per object
+        # a random factor is included to make it less predictable.
+        object_density = (30 / 9) * random.uniform(0.5, 2.0)
+
+        total_space = self.outer_limit - self.inner_limit
+        total_objects = total_space / object_density
+        return int(total_objects)
+
+    def estimate_distance(self, normalized_distance):
+        """
+        Estimates a planet's distance from the Sun based on a normalized distance.
+
+        Args:
+            normalized_distance (float): A value between 0 inner-most planet and 1 outer-most planet.
+
+        Returns:
+            float: The estimated distance from the main star in AU.
+        """
+        return 0.35 * 1.11**normalized_distance
+    
+    def __str__(self):
+        """
+        Generates a string output for the system data.
+        """
+        output = []
+        output.append(str(self.star))
+
+        for planet in self.planets:
+            output.append(str(planet))
+        
+        return output.join('\n')
